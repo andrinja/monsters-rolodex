3011a05ad76366c8f83232d4927463007b8ad7fa
andrinja <andra.zeberga@gmail.com>
2019-09-01 18:38:24 +0200

    Updates

---
+++ precache-manifest.3d8eb5c179f584fc5e186a58bcc00a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c60e01245fc0dda973e66fd3fc69e5e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "77bf2a6f37be5ea25ce4",
+    "url": "/monsters-rolodex/static/css/main.5777955e.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "77bf2a6f37be5ea25ce4",
+    "url": "/monsters-rolodex/static/js/main.ed9a36d6.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
